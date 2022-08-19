173559672a4aff57f5f3d024b242c6ef050648ac
h-bajramovic <78616962+h-bajramovic@users.noreply.github.com>
2022-08-19 15:52:17 -0500

    added user input for minterms

--- src/mccluskey.c
+++ src/mccluskey.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+
 int main() {
-    printf("hello world\n");
+
+    int num_minterms;
+    printf("Enter number of minterms: ");
+    scanf("%d", &num_minterms);
+
+    int minterms[num_minterms];
+    for (int i = 0; i < num_minterms; i++) {
+        printf("Enter minterm: ");
+        scanf("%d", minterms +i);
+    };
+    for (int i = 0; i < num_minterms; i++) {
+        printf("%d\n", minterms[i]);
+    };
     return 0;
 }
