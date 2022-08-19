eda1c671078d1160e8c720c5f71cf3005370a23f
h-bajramovic <78616962+h-bajramovic@users.noreply.github.com>
2022-08-19 16:24:55 -0500

    accepts minterms through main arguments in terminal

--- src/mccluskey.c
+++ src/mccluskey.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+    /*
+    * args to main are going to be length of minterms (argc -1)
+    * as well as argv which holds the minterms from index 1 to n
+    * Will go through argv to convert from char to int and place into new array
+    */
+   int num_minterms = argc - 1;
+   int minterms[num_minterms];
+   for (int i = 1; i < argc; i++) {
+       minterms[i - 1] = atoi(argv[i]);
+   };
 
-    int num_minterms;
-    printf("Enter number of minterms: ");
-    scanf("%d", &num_minterms);
-
-    int minterms[num_minterms];
-    for (int i = 0; i < num_minterms; i++) {
-        printf("Enter minterm: ");
-        scanf("%d", minterms +i);
-    };
-    for (int i = 0; i < num_minterms; i++) {
-        printf("%d\n", minterms[i]);
-    };
+   for (int i = 0; i < num_minterms; i++){
+       printf("Minterm #%d: %d\n", i, minterms[i]);
+   };
     return 0;
 }
