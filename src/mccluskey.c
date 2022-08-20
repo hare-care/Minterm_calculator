80be4bcda6acb8c6f010f5d73cc2270a926bc01c
h-bajramovic <78616962+h-bajramovic@users.noreply.github.com>
2022-08-20 08:46:14 -0500

    created bin, precursor to init minterm

--- src/mccluskey.c
+++ src/mccluskey.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    unsigned int num_true;
+    bool checked;
+    unsigned* array_ptr;
+} minterm_t;
+
+unsigned* bin(unsigned n, size_t length)
+{
+    int num_true = 0;
+    unsigned* array = malloc(length * sizeof(unsigned));
+    int count = 0;
+    unsigned i;
+    for (i = 1 << (length - 1); i > 0; i = i / 2) {
+        if (n & i) {
+            array[count] = 1;
+            num_true++;
+        } else {
+            array[count] = 0;
+        };
+        count++;
+    }
+    return array;
+}
+
+void initialize_minterm(int n) {
+    
+
+}
+
+/* Used to print array contents for testing */
+void print_array(unsigned* array_ptr, size_t length) {
+    for (int i = 0; i < length; i++) {
+        printf("Index %d: %d\n", i, array_ptr[i]);
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     /*
@@ -16,5 +54,14 @@ int main(int argc, char* argv[]) {
    for (int i = 0; i < num_minterms; i++){
        printf("Minterm #%d: %d\n", i, minterms[i]);
    };
+   unsigned* test_array = bin(0, 4);
+   print_array(test_array, 4);
+   free(test_array);
+   
+   
+
+
+
+   
     return 0;
 }
